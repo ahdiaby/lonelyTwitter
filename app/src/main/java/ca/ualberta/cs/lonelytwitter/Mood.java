754b762cbc069542f3efac30713f3cf493beaa8d
Harouna Diaby <harouna@cs.ualberta.ca>
2018-01-18 19:52:39 -0700

    LAB2

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Mood.java
@@ -0,0 +1,10 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by harouna on 1/18/18.
+ */
+
+public class Mood extends Tweet {
+    private
+
+}
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetTooLongException.java
@@ -0,0 +1,8 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by harouna on 1/18/18.
+ */
+
+public class TweetTooLongException extends Exception {
+}
